61ba99bf83d1167633a18b54e6ad912733ca8f0f
Ronda Yummy <basrers@ukr.net>
2022-12-25 00:51:50 +0200

    supportedFacingMode

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -14,6 +14,7 @@ export class AppComponent implements OnInit {
   selectedPhotos: Array<string> = [];
   continueListPhotos: Array<string> = [];
   handleCameraPosition: 'user' | 'environment' = 'user';
+  supportedFacingMode = navigator.mediaDevices.getSupportedConstraints().facingMode;
 
   takenPhotos: Array<string> = [];
   takenSelectedPhotos: Array<string> = [];
@@ -148,6 +149,7 @@ export class AppComponent implements OnInit {
 
   changeCamera() {
     if (this.handleCameraPosition === 'user') {
+      console.log('user');
       this.handleCameraPosition = 'environment';
       this.closeWindowForTakent();
       this.takePhotoUser();
