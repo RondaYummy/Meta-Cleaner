5c9eb165d7c847f8b1a304fb4e4c44242ada9473
Ronda Yummy <basrers@ukr.net>
2022-12-25 12:31:19 +0200

    add watermark

--- src/app/selected-photo/selected-photo.component.ts
+++ src/app/selected-photo/selected-photo.component.ts
@@ -1,16 +1,42 @@
-import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { Component, EventEmitter, Input, Output, OnInit } from '@angular/core';
+const watermark = require('watermarkjs');
 
 @Component({
   selector: 'app-selected-photo',
   templateUrl: './selected-photo.component.html',
   styleUrls: ['./selected-photo.component.scss'],
 })
-export class SelectedPhotoComponent {
+export class SelectedPhotoComponent implements OnInit {
   @Input() fileList: Array<string> = [];
   @Output() close = new EventEmitter<boolean>();
   @Output() clear = new EventEmitter<Array<string>>();
+  @Input() pasteWatermark = false;
+  @Input() selectValue: string = 'lowerRight';
+  savedImagesWithoutWatermark: Array<string>;
 
   constructor() {}
+
+  ngOnInit() {
+    this.savedImagesWithoutWatermark = this.fileList;
+  }
+
+  selectChanged(position: string) {
+    this.fileList = [];
+    this.savedImagesWithoutWatermark.forEach((file, index) => {
+      watermark([file, '../../assets/images/logo.png'])
+        .dataUrl(watermark.image[position](0.5))
+        .then((img: any) => {
+          this.fileList.push(img);
+        });
+    });
+  }
+
+  changeCheckbox() {
+    this.pasteWatermark = !this.pasteWatermark;
+    if (this.pasteWatermark) {
+      this.selectChanged('lowerRight');
+    }
+  }
 
   closeSelected() {
     this.close.emit(true);
