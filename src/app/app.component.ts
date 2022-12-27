85e9970b88954e50dbbb3419a0d49ff75446c6a3
Ronda Yummy <basrers@ukr.net>
2022-12-28 00:46:08 +0200

    Revert "Revert "scalable""
    This reverts commit 61c8b7d0fbfcbbc13e07a03db4c37ce24013deb3.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -14,7 +14,8 @@ export class AppComponent implements OnInit {
   selectedPhotos: Array<string> = [];
   continueListPhotos: Array<string> = [];
   handleCameraPosition: 'user' | 'environment' = 'user';
-  supportedFacingMode = navigator.mediaDevices.getSupportedConstraints().facingMode;
+  supportedFacingMode =
+    navigator.mediaDevices.getSupportedConstraints().facingMode;
 
   takenPhotos: Array<string> = [];
   takenSelectedPhotos: Array<string> = [];
@@ -38,6 +39,19 @@ export class AppComponent implements OnInit {
   constructor() {}
 
   ngOnInit() {
+    if (/iPad|iPhone|iPod/.test(navigator.userAgent)) {
+      const handler = (e: any) => {
+        if (e.scale !== 1) {
+          e.preventDefault();
+        }
+      };
+
+      window.addEventListener('touchmove', handler, { passive: false });
+      window.document.addEventListener('touchmove', handler, {
+        passive: false,
+      });
+    }
+
     window.addEventListener('offline', () => {
       this.isOnline = false;
     });
@@ -86,10 +100,10 @@ export class AppComponent implements OnInit {
   handleFiles(event: Event) {
     const target = event.target as HTMLInputElement;
     const files = target.files as FileList;
-      Array.from(files).forEach((file: File) => {
-        this.uploadFile(file);
-      });
-      target.value = '';
+    Array.from(files).forEach((file: File) => {
+      this.uploadFile(file);
+    });
+    target.value = '';
   }
 
   closeSelected(event: boolean) {
@@ -147,11 +161,11 @@ export class AppComponent implements OnInit {
       video: {
         facingMode: {
           exact: this.handleCameraPosition,
-        }
-      }
-    }
-    return constraints
-  };
+        },
+      },
+    };
+    return constraints;
+  }
 
   changeCamera() {
     if (this.handleCameraPosition === 'user') {
